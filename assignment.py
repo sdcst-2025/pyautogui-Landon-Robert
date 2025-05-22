e3842e9c183bf85a1e215f105cc3ed7957c100eb
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-05-22 11:33:31 -0700

    -

--- assignment.py
+++ assignment.py
@@ -1,7 +1,14 @@
 import webbrowser
 import pyautogui
-
+import keyboard
 
 webbrowser.open("https://orteil.dashnet.org/cookieclicker/")
 pyautogui.moveTo(1,1)
 
+run = True
+while run == True:
+    
+
+
+    if keyboard.is_pressed("q"):
+        break
