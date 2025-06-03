bc68cf14a23cced61c1de675e32cce70b3a88fcc
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-06-03 11:35:06 -0700

    worked on clicker

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ import webbrowser
 import pyautogui
 import keyboard
 import time
-
+"""
 #webbrowser.open("https://orteil.dashnet.org/cookieclicker/")
 run = 0
 while run < 30:
@@ -14,13 +14,22 @@ while run < 30:
     ## pyautogui.moveTo(10,10)
     ## time.sleep(0.01)
     run = run + 1
-
-
+"""
 
 run = True
 while run == True:
-    
-
-
+    cookiePos = pyautogui.locateOnScreen('Assets/cookie2.png', confidence=0.7)
+    if pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5) != None:
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5))
+        pyautogui.click()
+    elif pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.5) != None:
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.5))
+        pyautogui.click()
+    elif pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.5) != None:
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.5))
+        pyautogui.click()
+    else:
+        pyautogui.moveTo(cookiePos)
+        pyautogui.click(clicks=100, interval=0.1)
     if keyboard.is_pressed("q"):
         break
