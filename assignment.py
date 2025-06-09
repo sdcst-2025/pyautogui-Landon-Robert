20fa3690ef5207af35090b3e7f0638d5d63af9da
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-06-09 11:34:48 -0700

    -

--- assignment.py
+++ assignment.py
@@ -2,6 +2,7 @@ import webbrowser
 import pyautogui
 import keyboard
 import time
+
 """
 webbrowser.open("https://orteil.dashnet.org/cookieclicker/")
 """
@@ -10,19 +11,20 @@ run = True
 while run == True:
     cookiePos = pyautogui.locateOnScreen('Assets/cookie2.png', confidence=0.7)
     try:
-        pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5)
-        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5))
+        pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.7)
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.7))
         pyautogui.click()
     except:
         try:
-            pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.5)
-            pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.5))
+            pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.9)
+            pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.9))
             pyautogui.click()
+            time.sleep(0.1)
         except:
             try:
-                pyautogui.locateOnScreen("Assets/buttonActive2.png", confidence=0.5)
-                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive2.png", confidence=0.5))
-                pyautogui.click()
+                pyautogui.locateOnScreen("Assets/buttonActive4.png", confidence=0.9)
+                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive4.png", confidence=0.9))
+                pyautogui.click(clicks=10, interval=0.1)
             except:
                 pyautogui.moveTo(cookiePos)
                 pyautogui.click(clicks=100, interval=0.1)
