c9ea54c629ad4f392fad685863537cf0da7275e0
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-06-11 11:45:45 -0700

    -

--- assignment.py
+++ assignment.py
@@ -11,23 +11,22 @@ run = True
 cookiePos = pyautogui.locateOnScreen('Assets/cookie2.png', confidence=0.8)
 while run == True:
     try:
-        pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.7)
-        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.7))
+        pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5)
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5))
         pyautogui.click()
     except:
         try:
             pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.9)
             pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive2.png", confidence=0.9))
             pyautogui.click()
-            time.sleep(0.1)
         except:
             try:
-                pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.8)
-                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.8))
-                pyautogui.click(clicks=10, interval=0.1)
+                pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.9)
+                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.9))
+                pyautogui.click(clicks=10, interval=0.02)
             except:
                 pyautogui.moveTo(cookiePos)
-                pyautogui.click(clicks=100, interval=0.1)
+                pyautogui.click(clicks=100, interval=0.02)
 
     if keyboard.is_pressed("q"):
         run = False
