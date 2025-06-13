97236a9c599890bb97cdba30e2c6ba83f7a84d4a
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-06-13 11:35:49 -0700

    -

--- assignment.py
+++ assignment.py
@@ -11,18 +11,18 @@ run = True
 cookiePos = pyautogui.locateOnScreen('Assets/cookie2.png', confidence=0.8)
 while run == True:
     try:
-        pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5)
-        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie.png", confidence=0.5))
+        pyautogui.locateOnScreen("Assets/goldenCookie2.png", confidence=0.5)
+        pyautogui.moveTo(pyautogui.locateOnScreen("Assets/goldenCookie2.png", confidence=0.5))
         pyautogui.click()
     except:
         try:
-            pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.9)
-            pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive2.png", confidence=0.9))
-            pyautogui.click()
+            if pyautogui.locateOnScreen("Assets/upgradeActive.png", confidence=0.87):
+                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/upgradeActive2.png", confidence=0.87))
+                pyautogui.click()
         except:
             try:
-                pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.9)
-                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive.png", confidence=0.9))
+                pyautogui.locateOnScreen("Assets/buttonActive2.png", confidence=0.87)
+                pyautogui.moveTo(pyautogui.locateOnScreen("Assets/buttonActive2.png", confidence=0.9))
                 pyautogui.click(clicks=10, interval=0.02)
             except:
                 pyautogui.moveTo(cookiePos)
